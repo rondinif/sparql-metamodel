572606dc4e65349a7629ccbb94ba8fd4eaf887b2
Niels Christensen <nhc@mayacs.com>
2014-08-22 12:25:36 +0100

    Query dbpedia using query from a file

--- src/main/java/com/mayacs/experiments/Main.java
+++ src/main/java/com/mayacs/experiments/Main.java
@@ -1,34 +1,41 @@
 package com.mayacs.experiments;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.Scanner;
+
 import org.apache.metamodel.DataContext;
-import org.apache.metamodel.schema.Schema;
-import org.apache.metamodel.schema.Table;
-import org.apache.metamodel.schema.Column;
 import org.apache.metamodel.util.UrlResource;
 import org.apache.metamodel.csv.CsvConfiguration;
 import org.apache.metamodel.csv.CsvDataContext;
+import org.apache.metamodel.data.DataSet;
+import org.apache.metamodel.data.Row;
 
 public final class Main {
-
-    public static void main(String[] args) {
-        DataContext dataContext = new CsvDataContext(new UrlResource("http://dbpedia.org/sparql?default-graph-uri=http%3A%2F%2Fdbpedia.org&query=SELECT+*+WHERE+%7B%0D%0A%3Fid+a+foaf%3APerson+%3B%0D%0A++++foaf%3Aname+%3Fname+%3B%0D%0A++++foaf%3Adepiction+%3Fimgurl%0D%0A%7D%0D%0ALIMIT+100%0D%0AOFFSET+100&format=text%2Fcsv&timeout=30000&debug=on"), new CsvConfiguration());
-        Schema[] schemas = dataContext.getSchemas();  
-        log("Schemas");
-        for (final Schema s : schemas) {
-            log(s);
-        }
-        Table[] tables = schemas[1].getTables();  
-        log("Tables");
-        for (final Table s : tables) {
-            log(s);
-        }
-        Column[] columns = tables[0].getColumns();
-        log("Columns");
-        for (final Column s : columns) {
-            log(s);
+    private static final String FOAF_QUERY =
+        new Scanner(Main.class.getResourceAsStream("foaf.sparql.txt"), "UTF-8")
+            .useDelimiter("\\A")
+            .next();
+    
+    public static void main(String[] args) throws UnsupportedEncodingException {
+        DataContext dataContext = new CsvDataContext(new UrlResource(dbpediaUrl(FOAF_QUERY)), new CsvConfiguration());
+        DataSet dataSet = dataContext.query()
+            .from(dataContext.getSchemas()[1].getTable(0))
+            .selectAll()
+            .execute();
+        for (final Row row : dataSet) {
+            log(row);
         }
     }
 
+    private static String dbpediaUrl(final String query) throws UnsupportedEncodingException {
+        return String.format(
+                "http://dbpedia.org/sparql?default-graph-uri=%s&query=%s&format=%s&timeout=30000&debug=on", 
+                URLEncoder.encode("http://dbpedia.org", "UTF-8"),
+                URLEncoder.encode(query, "UTF-8"),
+                URLEncoder.encode("text/csv", "UTF-8"));
+    }
+    
     private static void log(final Object o) {
         System.out.println(o.toString());
     }
