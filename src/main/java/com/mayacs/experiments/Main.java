76905fb407d625e2fd48fb975b89a3eb888fda34
Niels Christensen <nhc@mayacs.com>
2014-08-22 14:04:16 +0100

    Use 2 different queries

--- src/main/java/com/mayacs/experiments/Main.java
+++ src/main/java/com/mayacs/experiments/Main.java
@@ -13,22 +13,25 @@ import org.apache.metamodel.data.DataSet;
 import org.apache.metamodel.data.Row;
 
 public final class Main {
-    private static final String FOAF_QUERY =
-        new Scanner(Main.class.getResourceAsStream("foaf.sparql.txt"), "UTF-8")
-            .useDelimiter("\\A")
-            .next();
+    private static final String TRIPLES_QUERY = loadResource("triples.sparql.txt");
+    private static final String FOAF_QUERY = loadResource("foaf.sparql.txt");
     
     public static void main(String[] args) throws UnsupportedEncodingException {
-        DataContext dataContext = new CsvDataContext(new UrlResource(dbpediaUrl(FOAF_QUERY)), new CsvConfiguration());
+        printResult(TRIPLES_QUERY);
+        System.out.println("================================================");
+        printResult(FOAF_QUERY);
+    }
+    
+    private static void printResult(final String query) throws UnsupportedEncodingException {
+        DataContext dataContext = new CsvDataContext(new UrlResource(dbpediaUrl(query)), new CsvConfiguration());
         DataSet dataSet = dataContext.query()
             .from(dataContext.getSchemas()[1].getTable(0))
             .selectAll()
             .execute();
         for (final Row row : dataSet) {
             System.out.println(Arrays.asList(row.getValues()));
-        }
+        }        
     }
-
     private static String dbpediaUrl(final String query) throws UnsupportedEncodingException {
         return String.format(
                 "http://dbpedia.org/sparql?default-graph-uri=%s&query=%s&format=%s&timeout=30000", 
@@ -37,4 +40,10 @@ public final class Main {
                 URLEncoder.encode("text/csv", "UTF-8"));
     }
     
+    private static String loadResource(final String name) {
+        final Scanner scan = new Scanner(Main.class.getResourceAsStream(name), "UTF-8");
+        final String result = scan.useDelimiter("\\A").next();
+        scan.close();
+        return result;
+    }
 }
